1e864c0c90f28bc355129015fdae843b4e459de5
joshimihir <mihirj@solguruz.com>
2023-02-13 11:14:46 +0530

    feat: creating Socket IO client class

---
+++ lib/services/socket_io_client.dart
@@ -0,0 +1,72 @@
+import 'package:get/get.dart';
+import 'package:socket_io_client/socket_io_client.dart';
+
+class SocketIOClient extends GetxService {
+  Socket? _socket;
+  RxBool isMicConnected = false.obs;
+  RxString socketResponseText = ''.obs;
+
+  void socketEmit(
+      {required String emittingStatus,
+      required dynamic emittingData,
+      required bool isDataToSend}) {
+    isDataToSend
+        ? _socket?.emit(emittingStatus, emittingData)
+        : _socket?.emit(emittingStatus);
+  }
+
+  void socketConnect({
+    required String apiCallbackURL,
+    required String languageCode,
+  }) {
+    _socket = io(
+        apiCallbackURL,
+        OptionBuilder()
+            .setTransports(['websocket', 'polling']) // for Flutter or Dart VM
+            .disableAutoConnect()
+            .setQuery({
+              'language': languageCode,
+              'EIO': 4,
+              'transport': 'websocket',
+            })
+            .build());
+
+    setSocketMethods();
+    _socket?.connect();
+  }
+
+  void disconnect() {
+    _socket?.close();
+    isMicConnected.value = false;
+  }
+
+  void setSocketMethods() {
+    _socket?.onConnect((receivedData) {});
+
+    _socket?.on('connect-success', (data) {
+      isMicConnected.value = true;
+      print('socket: $data');
+    });
+
+    _socket?.on('response', (data) {
+      if (data is List && data.isNotEmpty && data[0].isNotEmpty) {
+        socketResponseText.value = data[0];
+      }
+      print('socket: $data');
+    });
+
+    _socket?.on('terminate', (data) {
+      isMicConnected.value = false;
+      print('socket: $data');
+    });
+
+    _socket?.onDisconnect((data) {
+      isMicConnected.value = false;
+      print('socket: $data');
+    });
+  }
+
+  bool isConnected() {
+    return _socket != null && _socket!.connected;
+  }
+}
