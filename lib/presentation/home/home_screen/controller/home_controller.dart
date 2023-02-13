c7c55c2c8b9d5b5ae4aa4b9cbc681519fc524eb6
joshimihir <mihirj@solguruz.com>
2023-02-13 11:22:05 +0530

    feat: source/target languages based on ASR pref

--- lib/presentation/home/home_screen/controller/home_controller.dart
+++ lib/presentation/home/home_screen/controller/home_controller.dart
@@ -1,8 +1,10 @@
 import 'package:get/get.dart';
+import 'package:hive/hive.dart';
 
 import '../../../../common/controller/language_model_controller.dart';
 import '../../../../services/translation_app_api_client.dart';
 import '../../../../utils/constants/api_constants.dart';
+import '../../../../utils/constants/app_constants.dart';
 import '../../../../utils/snackbar_utils.dart';
 
 class HomeController extends GetxController {
@@ -12,10 +14,13 @@ class HomeController extends GetxController {
   late TranslationAppAPIClient _translationAppAPIClient;
   late LanguageModelController _languageModelController;
 
+  late final Box _hiveDBInstance;
+
   @override
   void onInit() {
     _translationAppAPIClient = Get.find();
     _languageModelController = Get.find();
+    _hiveDBInstance = Hive.box(hiveDBName);
 
     super.onInit();
   }
@@ -38,7 +43,8 @@ class HomeController extends GetxController {
         await _translationAppAPIClient.getAllModels(taskPayloads: taskPayloads);
     allModelResponse.when(
       success: ((data) {
-        _languageModelController.calcAvailableSourceAndTargetLanguages(data);
+        _languageModelController.calcAvailableSourceAndTargetLanguages(
+            data, _hiveDBInstance.get(isStreamingPreferred));
         isModelsLoading.value = false;
       }),
       failure: (error) {
