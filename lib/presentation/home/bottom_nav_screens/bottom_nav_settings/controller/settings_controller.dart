7cb81fef4b225d6ff8748ca8fb5f7c8c8f585856
joshimihir <mihirj@solguruz.com>
2023-02-20 18:49:52 +0530

    feat: reset result UI when streaming pref changed

--- lib/presentation/home/bottom_nav_screens/bottom_nav_settings/controller/settings_controller.dart
+++ lib/presentation/home/bottom_nav_screens/bottom_nav_settings/controller/settings_controller.dart
@@ -7,6 +7,7 @@ import '../../../../../enums/gender_enum.dart';
 import '../../../../../enums/language_enum.dart';
 import '../../../../../utils/constants/api_constants.dart';
 import '../../../../../utils/constants/app_constants.dart';
+import '../../bottom_nav_translation/controller/bottom_nav_translation_controller.dart';
 
 class SettingsController extends GetxController {
   RxBool isTransLiterationEnabled = true.obs;
@@ -56,5 +57,7 @@ class SettingsController extends GetxController {
     LanguageModelController languageModelController = Get.find();
     languageModelController.calcAvailableSourceAndTargetLanguages(
         isStreamingPreferred: isEnabled);
+    BottomNavTranslationController bottomNavTranslationController = Get.find();
+    bottomNavTranslationController.resetAllValues();
   }
 }
