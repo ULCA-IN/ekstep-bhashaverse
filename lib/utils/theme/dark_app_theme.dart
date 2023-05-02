2b1b35ed8526ea9b89fdab3c6ed4fa0f6a8e1b60
mihirjsolguruz <mihirj@solguruz.com>
2023-05-02 22:25:23 +0530

    feat: adding light and dark theme

---
+++ lib/utils/theme/dark_app_theme.dart
@@ -0,0 +1,92 @@
+part of app_theme;
+
+class DarkAppTheme implements AppTheme {
+  const DarkAppTheme();
+
+  @override
+  Color get backgroundColor => _AppColors.jaguarBlack;
+
+  @override
+  Color get buttonSelectedColor => _AppColors.tangerineOrangeColor;
+
+  @override
+  Color get containerColor => _AppColors.jaguarBlue;
+
+  @override
+  Color get disabledBGColor => _AppColors.americanSilver.withOpacity(0.1);
+
+  @override
+  Color get disabledTextColor => _AppColors.americanSilver;
+
+  @override
+  Color get errorColor => _AppColors.brickRed;
+
+  @override
+  Color get highlightedBGColor => _AppColors.japaneseLaurel;
+
+  @override
+  Color get textFieldBorderColor => _AppColors.magicMint.withOpacity(0.1);
+
+  @override
+  Color get highlightedTextColor => _AppColors.mischkaGrey;
+
+  @override
+  Color get hintTextColor => _AppColors.mischkaGrey;
+
+  @override
+  Color get lightBGColor => _AppColors.bastilleBlack;
+
+  @override
+  Color get orangeBGColor => _AppColors.bisqueOrangeColor;
+
+  @override
+  Color get primaryColor => _AppColors.primaryDarkColor;
+
+  @override
+  Color get primaryTextColor => _AppColors.goastWhite;
+
+  @override
+  Color get readOnlyTextColor => _AppColors.lilyGrey;
+
+  @override
+  Color get secondaryTextColor => _AppColors.dolphinGray;
+
+  @override
+  Color get titleTextColor => _AppColors.manateeGray;
+
+  @override
+  Color get transliterationTextColor => _AppColors.lilyWhite;
+
+  @override
+  Color get warningColor => _AppColors.frolyRed;
+
+  @override
+  Color get disabledOrangeColor => _AppColors.flushOrangeColor;
+
+  @override
+  Color get cardBGColor => _AppColors.bastilleBlack;
+
+  @override
+  Color get hightlitedTextFeildColor => _AppColors.magicMint.withOpacity(0.1);
+
+  @override
+  Color get normalTextFeildColor => _AppColors.jaguarBlue;
+
+  @override
+  Color get iconOutlineColor => _AppColors.balticSea;
+
+  @override
+  Color get disabledIconOutlineColor => _AppColors.brightGrey;
+
+  @override
+  Color get highlightedBorderColor => _AppColors.primaryDarkColor;
+
+  @override
+  Color get listingScreenBGColor => _AppColors.jaguarBlack;
+
+  @override
+  Color get speackerColor => _AppColors.haitiBlack;
+
+  @override
+  Color get voiceAssistantBGColor => _AppColors.bastilleBlack;
+}
