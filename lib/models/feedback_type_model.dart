f675ac9f32d46459d1bd6eb24234b39b2a75e473
mihirjsolguruz <mihirj@solguruz.com>
2023-05-23 16:59:28 +0530

    feat: adding feedback module

---
+++ lib/models/feedback_type_model.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class FeedbackTypeModel {
+  final String taskType;
+  final String question;
+  final TextEditingController textController;
+  final FocusNode focusNode;
+  RxDouble taskRating;
+  final List<GranularFeedback> granularFeedbacks;
+
+  FeedbackTypeModel({
+    required this.taskType,
+    required this.question,
+    required this.textController,
+    required this.focusNode,
+    required this.taskRating,
+    required this.granularFeedbacks,
+  });
+}
+
+class GranularFeedback {
+  final String question;
+  final List<dynamic> supportedFeedbackTypes;
+  final double mainRating;
+  final List<dynamic> parameters;
+
+  GranularFeedback({
+    required this.question,
+    required this.supportedFeedbackTypes,
+    required this.parameters,
+    required this.mainRating,
+  });
+}
+
+class Parameter {
+  final String paramName;
+  final double paramRating;
+
+  Parameter({
+    required this.paramName,
+    required this.paramRating,
+  });
+}
